5a1c112054ee99bbeff90bb47e5c5eccaa69c1e0
IamSoPrada <9999goldcoin@gmail.com>
2020-07-08 18:59:39 +0400

    Added navigation

--- public/js/scriptGSAP.js
+++ public/js/scriptGSAP.js
@@ -1,3 +1,19 @@
+gsap.registerPlugin(ScrollToPlugin);
+
+const projectSection = document.querySelector("#projects");
+const about = document.querySelector("#about");
+const btn = document.querySelector(".contact-btn");
+
+projectSection.addEventListener("click", function (e) {
+    e.preventDefault();
+    gsap.to(window, { duration: 1, scrollTo: ".main-projects" });
+})
+
+about.addEventListener("click", function (e) {
+    e.preventDefault();
+    gsap.to(window, { duration: 1, scrollTo: ".main-about" });
+})
+
 gsap.from(".fab", {
     duration: 2,
     scale: 0.3,
@@ -8,7 +24,7 @@ gsap.from(".fab", {
     force3D: true
 });
 
-gsap.fromTo(".promo-title", {x: 0, y: -1000, opacity: 0.2 }, {x: 0, y: 0,opacity: 1, duration: 2, ease: "power1"});
+gsap.fromTo(".promo-title", { x: 0, y: -1000, opacity: 0.2 }, { x: 0, y: 0, opacity: 1, duration: 2, ease: "power1" });
 
 document.querySelectorAll(".fab").forEach(function (fab) {
     fab.addEventListener("click", function () {
